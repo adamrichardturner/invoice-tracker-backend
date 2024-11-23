e10afb1c673a526dbb2d1c0b11a8a67ee97f9093
adamrichardturner <hello@adamrichardturner.dev>
2024-11-23 10:07:31 +0000

    CORs issue

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -57,9 +57,9 @@ router.post("/demo-login", async (req: Request, res: Response) => {
         res.cookie("token", token, {
             domain: ".adamrichardturner.dev",
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
-            maxAge: 24 * 60 * 60 * 1000, // 1 day
+            secure: true,
+            sameSite: "none",
+            maxAge: 24 * 60 * 60 * 1000,
         });
 
         return res.json({ success: true });
